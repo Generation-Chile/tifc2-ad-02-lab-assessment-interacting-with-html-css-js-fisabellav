275464d72b4498dfa8a099b1b339990ee980f49a
Fernanda Valenzuela <fernandaisabellavalenzuelac@gmail.com>
2025-04-24 11:20:04 -0400

    modificaciones css, html y js

---
+++ script.js
@@ -0,0 +1,7 @@
+const header2 = document.getElementById("rojo");
+header2.textContent =  "Adiós";
+document.getElementById("header3").style.color = "orange";
+
+header2.onclick = function() {
+    header2.style.color = "brown";
+}
